e676636e7c2f51213e38e6b95f1cfa8123648865
Manoj Poudel <februarymanoj2004@gmail.com>
2025-05-15 11:24:47 +0545

    Auth updates and some new endpoints addition

--- Backend/src/main/java/com/MediSys/MediSys/controller/DoctorController.java
+++ Backend/src/main/java/com/MediSys/MediSys/controller/DoctorController.java
@@ -1,13 +1,20 @@
 package com.MediSys.MediSys.controller;
 
 import com.MediSys.MediSys.auth.dto.RegisterDoctorDto;
+import com.MediSys.MediSys.auth.model.User;
+import com.MediSys.MediSys.auth.repository.UserRepository;
 import com.MediSys.MediSys.exception.ResourceNotFoundException;
 import com.MediSys.MediSys.model.Doctor;
+import com.MediSys.MediSys.model.Patient;
+import com.MediSys.MediSys.repository.DoctorRepository;
+import com.MediSys.MediSys.repository.PatientRepository;
 import com.MediSys.MediSys.service.DoctorService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,10 +27,27 @@ public class DoctorController {
 
     @Autowired
     private DoctorService doctorService;
+    @Autowired
+    private DoctorRepository doctorRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping
     public List<Doctor> getAllDoctors() {
         return doctorService.getAllDoctors();
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getCurrentDoctor() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        Doctor doctor = doctorRepository.findByUserId(user.getId())
+                .orElseThrow(() -> new RuntimeException("Doctor not found"));
+
+        return ResponseEntity.ok(doctor);
     }
 
     @GetMapping("/{id}")
@@ -55,4 +79,5 @@ public class DoctorController {
         doctorService.deleteDoctor(id);
         return ResponseEntity.noContent().build();
     }
+
 }
